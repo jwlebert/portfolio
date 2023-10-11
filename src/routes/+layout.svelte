3b82613fb94b9a3fb68d2293cfc9a6a6749062fc
jwlebert <j.lebert4402@gmail.com>
2023-10-10 21:54:51 -0400

    added basic navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<style>
+    nav {
+        width: 100vw;
+        height: 8vh;
+        background-color: tan;
+
+        display: flex;
+        justify-content: center;
+    }
+
+    nav > ul {
+        display: flex;
+        
+        flex-grow: 0.5;
+        align-items: center;
+
+        margin: 0;
+        padding: 0;
+    }
+
+    nav > ul > li {
+        flex-grow: 1;
+        list-style-type: none;
+        text-align: center;
+    }
+</style>
+
+<nav>
+    <ul>
+        <li><a href = "/">Home</a></li>
+        <li><a href = "/">Home</a></li>
+        <li><a href = "/">Home</a></li>
+        <li><a href = "/">Home</a></li>
+        <li><a href = "/">Home</a></li>
+    </ul>
+</nav>
+
+<slot />
